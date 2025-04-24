2adfe071bd60721b384cbaf9a89264f9bf70e95c
Mauro Jeandet <40285566+maurojeandet@users.noreply.github.com>
2025-04-24 16:12:23 -0300

    Update example.spec.js

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,6 +2,8 @@
 import { test, expect } from '@playwright/test'
 import downloadLinks from './links.js'
 
+const downloadFolder = 'E:\\Torrents\\'
+
 test.setTimeout(1000*60*60*3)
 test('download links', async ({ page }) => {
   for (const dl of downloadLinks) {
@@ -10,6 +12,6 @@ test('download links', async ({ page }) => {
     await page.evaluate('seen = true; download();')
     const download = await downloadPromise;
     // Wait for the download process to complete and save the downloaded file somewhere.
-    await download.saveAs('E:\\Torrents\\' + download.suggestedFilename());
+    await download.saveAs(downloadFolder + download.suggestedFilename());
   }
 })
