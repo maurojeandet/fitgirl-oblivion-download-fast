054b891c2255a84783e16ec7c2a8136318b9115c
Mauro Jeandet <mauro.jeandet@gmail.com>
2025-04-24 16:33:48 -0300

    improve download

--- jobs/oblivion-fitgirl.spec.js
+++ jobs/oblivion-fitgirl.spec.js
@@ -7,13 +7,14 @@ const downloadFolder = 'C:\\Downloads\\'
 // set timeout to 3 hours
 test.setTimeout(1000*60*60*3)
 
-test('download parts', async ({ page }) => {
-  for (const dl of downloadLinks) {
+for (const dl of downloadLinks) {
+  const part = dl.split('_.part')[1]
+  test(`Download ${part ?? dl}`, async ({ page }) => {
     await page.goto(dl)
     const downloadPromise = page.waitForEvent('download');
     await page.evaluate('seen = true; download();')
-    const download = await downloadPromise;
+    const download = await downloadPromise
     // Wait for the download process to complete and save the downloaded file somewhere.
-    await download.saveAs(downloadFolder + download.suggestedFilename());
-  }
-})
+    await download.saveAs(downloadFolder + download.suggestedFilename())
+  })
+}
